94bea8bb3a3a32bbc6b4e94f3d055d2b066b26fc
Leonardo Malcotti <leo@Leonardos-MacBook-Air.local>
2024-07-24 17:07:26 +0200

    added implementation

--- src/main/java/com/sittingspot/searchservice/controller/SearchServiceController.java
+++ src/main/java/com/sittingspot/searchservice/controller/SearchServiceController.java
@@ -1,24 +1,70 @@
 package com.sittingspot.searchservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sittingspot.searchservice.model.Area;
 import com.sittingspot.searchservice.model.Location;
 import com.sittingspot.searchservice.model.QueryResult;
 import com.sittingspot.searchservice.model.Tag;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.UUID;
 
 @RestController("search-logic/api/v1")
 public class SearchServiceController {
 
+    @Value("${sittingspot.queryoptimizer.host}")
+    private String queryOptimizerHost;
+
+    @Value("${sittingspot.queryoptimizer.port}")
+    private String queryOptimizerPort;
+
+    @Value("${sittingspot.searchadapter.host}")
+    private String searchAdapterHost;
+
+    @Value("${sittingspot.searchadapter.port}")
+    private String searchAdapterPort;
+
     @GetMapping("/")
     public List<QueryResult> search(@RequestParam("queryId")UUID queryID,
                                     @RequestParam("location") Area location,
                                     @RequestParam(value = "tags",required = false) List<Tag> tags,
-                                    @RequestParam(value = "labels",required = false) List<String> labels) {
-        return List.of();
+                                    @RequestParam(value = "labels",required = false) List<String> labels) throws IOException, InterruptedException {
+        var client = HttpClient.newHttpClient();
+
+        var optimizeRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://" + queryOptimizerHost + ":" + queryOptimizerPort +
+                        "/?queryId="+queryID +"&location="+location+"&tags="+tags+"&labels="+labels))
+                .GET().build();
+        var optimizeResult = client.send(optimizeRequest, HttpResponse.BodyHandlers.ofString());
+
+        if (optimizeResult.statusCode() == 200) {
+            List<QueryResult> data = (new ObjectMapper()).readerForListOf(QueryResult.class).readValue(optimizeResult.body());
+            return data;
+        }
+
+        var searchRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://"+ searchAdapterHost + ":" + searchAdapterPort +
+                        "/?location="+location+"&tags="+tags+"&labels="+labels))
+                .GET()
+                .build();
+        var searchResult = client.send(searchRequest, HttpResponse.BodyHandlers.ofString());
+
+        if (searchResult.statusCode() != 200) {
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE);
+        }
+
+        List<QueryResult> data = (new ObjectMapper()).readerForListOf(QueryResult.class).readValue(searchResult.body());
+        return data;
     }
 }
