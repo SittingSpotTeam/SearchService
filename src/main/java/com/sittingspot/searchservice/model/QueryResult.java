77edc3bcd3dfff51cc311715ed8cf8deba315f41
leo <leonardo.malcotti@studenti.unitn.it>
2024-08-15 00:59:15 +0200

    fixed pom and models

--- src/main/java/com/sittingspot/searchservice/model/QueryResult.java
+++ src/main/java/com/sittingspot/searchservice/model/QueryResult.java
@@ -1,6 +1,4 @@
 package com.sittingspot.searchservice.model;
 
-import java.util.UUID;
-
 public record QueryResult(String spotId, Location location) {
 }
--- src/main/java/com/sittingspot/searchservice/model/Tag.java
+++ src/main/java/com/sittingspot/searchservice/model/Tag.java
@@ -1,7 +1,4 @@
 package com.sittingspot.searchservice.model;
 
-import jakarta.persistence.Embeddable;
-
-@Embeddable
 public record Tag(String key, String value) {
 }
